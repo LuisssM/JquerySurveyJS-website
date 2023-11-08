0144a95c5c6459cf3535e4039169f609531e3b54
LuisssM <luispsmiranda.96@gmail.com>
2023-11-08 13:24:56 +0000

    first jquery survey.js

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ survey.onComplete.add(alertResults);
 $(function() {
     $("#surveyCont").Survey({ model: survey });
 });
+
+/* Caso queira guardar num server --->
+
+const SURVEY_ID = 1;
+
+function surveyComplete (sender) {
+    saveSurveyResults(
+        "https://your-web-service.com/" + SURVEY_ID,
+        sender.data
+    )
+}
+
+function saveSurveyResults(url, json) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        body: JSON.stringify(json)
+    })
+    .then(response => {
+        if (response.ok) {
+            // Handle success
+        } else {
+            // Handle error
+        }
+    })
+    .catch(error => {
+        // Handle error
+    });
+}
+
+const survey = new Survey.Model(surveyJson);
+
+survey.onComplete.add(surveyComplete);
+*/
